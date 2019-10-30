bb00d8983efb0fdf9fcf64678112b4880ba1408d
unknown <jessie.stieger@pulselabs.ai>
2019-10-30 15:08:20 -0700

    First commit

---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: "plain boring get something else"
+  },
+  {
+    name: 'Xploding Android',
+    price: .05,
+    description: "they won't let you bring this on a plane"
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
